b97130533e460b25bd0f72a535d58ea55064d1dc
Devi Sandeep Endluri V V <dendluri@codeaurora.org>
2017-10-30 10:24:53 +0530

    Add priv-app permissions to whitelist.
    Added priv-app to whitelist so as to provide priv app permissions.
    
    CRs-Fixed: 2131551
    Change-Id: I0f2446e1946cdd7cf8b1641a46818317acfaefdd

--- device.mk
+++ device.mk
@@ -130,7 +130,8 @@ PRODUCT_BOOT_JARS += \
 
 # Whitelisted apps
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
     
 # WiFi
 PRODUCT_PACKAGES += \
