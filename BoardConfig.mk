516bde6cfc67f9d454c395f06bcd9910d8bf45df
Anton Hansson <hansson@google.com>
2018-11-07 16:09:35 +0000

    sdm845-common: Remove obsolete dex settings
    This block is completely redundant because..
    - WITH_DEXPREOPT := true is the default
    - WITH_DEXPREOPT_PIC is never read
    - DEX_PREOPT_DEFAULT := nostripping has no effect when the
      BOARD_USES_SYSTEM_OTHER_ODEX flag is set to true
    
    Test: make
    Change-Id: I618a1e395ca1a14555cd50eef1a4f1aa079b553d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -63,14 +63,6 @@ TARGET_USE_QTI_BT_STACK := true
 # Camera
 TARGET_USES_QTI_CAMERA_DEVICE := true
 
-# Dex
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
-    WITH_DEXPREOPT ?= true
-  endif
-endif
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY ?= true
-
 # Display
 TARGET_SCREEN_DENSITY := 440
 
