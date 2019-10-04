f132ecbf51afe9dae43974b03b440606d4cb2d17
SagarMakhar <sagarmakhar@gmail.com>
2019-10-04 13:42:31 +0000

    RMX1921: Import sensors impl
    * as it is from hardware/interfaces
    * @pjgowtham :We are building vendor variants of the hal
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -248,7 +248,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-impl.RMX1921 \
     android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
