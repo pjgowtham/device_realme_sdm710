f3c033aa40f2e1ca6e8bffdfe7e08c35d97e798c
pjgowtham <pjgowtham@gmail.com>
2020-07-24 23:29:52 +0530

    RMX1921 : Build libgui_vendor from source

--- device.mk
+++ device.mk
@@ -147,6 +147,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64  \
     vendor.qti.hardware.camera.device@1.0.vendor \
     libstdc++.vendor \
+    libgui_vendor \
     Snap
 
 # Context Hub
