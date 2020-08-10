DerpFest-Devices/device_realme_RMX1921

RMX1921: Add assertions on TrustZone version
We have the rest of the assertion setup before hand

recovery/Android.mk
@@ -1,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_SRC_FILES := recovery_updater.cpp
+LOCAL_MODULE := librecovery_updater_RMX1851
+LOCAL_MODULE_TAGS := eng
+include $(BUILD_STATIC_LIBRARY)

pjgowtham/device_realme_sdm710

recovery/Android.mk
@@ -1,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_SRC_FILES := recovery_updater.cpp
+LOCAL_MODULE := librecovery_updater_RMX1921
+LOCAL_MODULE_TAGS := eng
+include $(BUILD_STATIC_LIBRARY)