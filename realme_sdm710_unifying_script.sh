cea61f7149c855dfb3042a58c3103c22c4e1fe2a
pjgowtham <pjgowtham@gmail.com>
2020-08-23 02:03:40 +0530

    realme_sdm710 : Realme 5 pro has pinkish tint when using camera
    Let's trying fixing it this way.

--- realme_sdm710_unifying_script.sh
+++ realme_sdm710_unifying_script.sh
@@ -62,7 +62,7 @@ else
     echo "Done removing NFC feature"
 fi
 
-# Remove GW1 camera blobs for Realme 5 Pro
+# Fix camera blobs for Realme 5 pro
 if [ "$RMX_PEAFOWL" = "RMX1971" ] ; then
     echo "Attempting to fix camera libs"
     rm -rf /vendor/lib64/camera/com.qti.sensormodule.qtech_s5kgw1.bin
@@ -70,5 +70,13 @@ if [ "$RMX_PEAFOWL" = "RMX1971" ] ; then
     rm -rf /vendor/lib64/camera/com.qti.sensormodule.sunny_s5kgw1_evt0_2.bin
     rm -rf /vendor/lib64/camera/com.qti.tuned.qtech_s5kgw1.bin
     rm -rf /vendor/lib64/camera/com.qti.tuned.sunny_s5kgw1.bin
+    rm -rf /vendor/lib64/camera/com.qti.tuned.holitech_gc2375h.bin
+    rm -rf /vendor/lib64/camera/com.qti.tuned.sunny_hi846.bin
+    rm -rf /vendor/lib64/camera/com.qti.tuned.truly_imx471.bin
+    rm -rf /vendor/lib64/camera/libarcsoft_low_light_hdr.so
+    mv /vendor/lib64/camera/5p_com.qti.tuned.holitech_gc2375h.bin /vendor/lib64/camera/com.qti.tuned.holitech_gc2375h.bin
+    mv /vendor/lib64/camera/5p_com.qti.tuned.sunny_hi846.bin /vendor/lib64/camera/com.qti.tuned.sunny_hi846.bin
+    mv /vendor/lib64/camera/5p_com.qti.tuned.truly_imx471.bin /vendor/lib64/camera/com.qti.tuned.truly_imx471.bin
+    mv /vendor/lib64/camera/5p_libarcsoft_low_light_hdr.so /vendor/lib64/camera/libarcsoft_low_light_hdr.so
     echo "Done fixing camera libs"
 fi
