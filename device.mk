d21714b7211d59345f2624019014ab226e1fc470
pjgowtham <pjgowtham@gmail.com>
2020-09-19 20:54:23 +0530

    RMX1921EU : Make NFC feature device specific
    * Stock kernel has support for nodes to convert the nfc feature device specific and we are simply utiilzing that with this implementation
    * We are creating that symlink file and the kernel node should take care of the rest
    * We have not imported the stock permissions as is and instead collated all ther permissions xml into one file
    Necessary but not dependent : https://github.com/pjgowtham/device_realme_sdm710/blob/lineage-17.1-oss-vendor/odm_manifests/manifest_RMX1921EU.xml

--- device.mk
+++ device.mk
@@ -300,11 +300,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
+    $(LOCAL_PATH)/nfc/com.oppo.nfc_feature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/nfc/com.oppo.nfc_feature.xml
     
 # Properties
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
