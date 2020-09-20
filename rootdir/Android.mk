352c51ac387f79280557a331a8abfd013f28f93f
pjgowtham <pjgowtham@gmail.com>
2020-09-20 22:53:25 +0530

    realme_sdm710 : devicesettings : Update rootdir script to exist in vendor

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -126,5 +126,5 @@ LOCAL_MODULE       := init.devicesetting.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.devicesetting.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
