1cc9bcf207b390ae3ffb6b26bbe38ec766a13d31
kaderbava <ksbava7325@gmail.com>
2020-09-23 00:26:00 +0530

    realme_sdm710 : Update to coral september R fingerprint

--- lineage_realme_sdm710.mk
+++ lineage_realme_sdm710.mk
@@ -20,7 +20,7 @@ PRODUCT_MODEL := realme_sdm710
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RP1A.200720.009 6720564 release-keys"
     
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
