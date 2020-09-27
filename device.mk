b7159f9f118c69b7c6a8a36d4f083fa8b01c1f51
Peter Cai <peter@typeblog.net>
2020-09-27 19:01:04 +0530

    realme_sdm710 : Override adb props for eng builds

--- device.mk
+++ device.mk
@@ -4,6 +4,15 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Adb
+ifeq ($(TARGET_BUILD_VARIANT),eng)
+# /vendor/default.prop is force-setting ro.adb.secure=1
+# Get rid of that by overriding it on eng builds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.secure=0 \
+    ro.adb.secure=0
+endif
+
 # Launched with pie
 $(call inherit-product, build/make/target/product/product_launched_with_p.mk)
 
