28fb2f5792cd3a093b115cb77f791a05f55eef02
pjgowtham <pjgowtham@gmail.com>
2020-12-19 17:44:43 +0530

    realme_sdm710 : Bump vendor security patch

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,7 +175,7 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/firmware_mnt:/firmware
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-06-05
+VENDOR_SECURITY_PATCH := 2020-11-05
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
