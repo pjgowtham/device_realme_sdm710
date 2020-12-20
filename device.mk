28e2b6fb04a984b74a240da67e638ccf377c4a57
pjgowtham <pjgowtham@gmail.com>
2020-12-20 16:39:28 +0530

    realme_sdm710 : remove trust hal

--- device.mk
+++ device.mk
@@ -407,10 +407,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.realme_sdm710
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
