99b40f98f6ff34960f64cd3d60b657bc5d63be1b
pjgowtham <pjgowtham@gmail.com>
2021-06-15 00:22:19 +0530

    realme_sdm710 : Sync props with stock
    *Remove unused props
    * Move around/add new props from stock ROM.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,34 +1,57 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES +=  \
     af.fast_track_multiplier=1 \
-    persist.vendor.audio.ambisonic.auto.profile=false \
+    vendor.audio_hal.period_size=192 \
     persist.vendor.audio.ambisonic.capture=false \
-    persist.vendor.audio.avs.afe_api_version=2 \
-    persist.vendor.audio.fluence.audiorec=false \
-    persist.vendor.audio.fluence.speaker=true \
-    persist.vendor.audio.fluence.tmic.enabled=false \
+    persist.vendor.audio.ambisonic.auto.profile=false \
+    ro.vendor.audio.sdk.fluencetype=none \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicerec=false \
-    persist.vendor.audio.hifi.int_codec=true \
-    persist.vendor.audio.ras.enabled=false \
-    persist.vendor.audio.spv3.enable=true \
-    ro.vendor.audio.sdk.fluencetype=none \
-    ro.vendor.audio.sdk.ssr=false \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.audiorec=false \
+    persist.vendor.audio.fluence.tmic.enabled=false \
     vendor.audio.apptype.multirec.enabled=false \
+    vendor.audio.record.multiple.enabled=false \
+    persist.vendor.audio.spv3.enable=true \
+    persist.vendor.audio.avs.afe_api_version=2 \
+    vendor.audio.tunnel.encode=false \
+    persist.vendor.audio.ras.enabled=false \
+    vendor.audio.offload.buffer.size.kb=64 \
+    audio.offload.min.duration.secs=30 \
+    audio.offload.video=true \
+    vendor.audio.offload.track.enable=true \
+    audio.deep_buffer.media=true \
+    vendor.voice.path.for.pcm.voip=true \
+    vendor.audio.offload.multiaac.enable=true \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.passthrough=false \
+    ro.vendor.audio.sdk.ssr=false \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.safx.pbe.enabled=false \
+    vendor.audio.parser.ip.buffer.size=262144 \
+    vendor.audio.flac.sw.decoder.24bit=true \
+    vendor.audio.hal.boot.timeout.ms=20000 \
+    vendor.audio.use.sw.alac.decoder=true \
+    vendor.audio.use.sw.ape.decoder=true \
+    vendor.audio.hw.aac.encoder=true \
+    persist.vendor.audio.hifi.int_codec=true \
+    ro.af.client_heap_size_kbyte=7168 \
+    vendor.audio.keep_alive.disabled=false \
+    vendor.audio.volume.headset.gain.depcal=true \
+    persist.vendor.audio.fluence.voicecomm=true \
+    persist.vendor.bt.aac_frm_ctl.enabled=true \
     vendor.audio.feature.a2dp_offload.enable=true \
     vendor.audio.feature.afe_proxy.enable=true \
     vendor.audio.feature.anc_headset.enable=true \
-    vendor.audio.feature.audiozoom.enable=false \
     vendor.audio.feature.battery_listener.enable=false \
     vendor.audio.feature.compr_cap.enable=false \
-    vendor.audio.feature.compr_voip.enable=false \
     vendor.audio.feature.compress_in.enable=false \
     vendor.audio.feature.compress_meta_data.enable=true \
+    vendor.audio.feature.compr_voip.enable=false \
     vendor.audio.feature.concurrent_capture.enable=false \
     vendor.audio.feature.custom_stereo.enable=true \
-    vendor.audio.feature.deepbuffer_as_primary.enable=false \
     vendor.audio.feature.display_port.enable=true \
     vendor.audio.feature.dsm_feedback.enable=false \
     vendor.audio.feature.dynamic_ecns.enable=false \
@@ -43,39 +66,24 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     vendor.audio.feature.hfp.enable=true \
     vendor.audio.feature.hifi_audio.enable=true \
     vendor.audio.feature.hwdep_cal.enable=false \
-    vendor.audio.feature.incall_music.enable=false \
+    vendor.audio.feature.incall_music.enable=true \
+    vendor.audio.feature.multi_voice_session.enable=true \
     vendor.audio.feature.keep_alive.enable=false \
     vendor.audio.feature.kpi_optimize.enable=true \
     vendor.audio.feature.maxx_audio.enable=false \
-    vendor.audio.feature.multi_voice_session.enable=true \
     vendor.audio.feature.ras.enable=true \
     vendor.audio.feature.record_play_concurency.enable=false \
-    vendor.audio.feature.snd_mon.enable=true \
-    vendor.audio.feature.spkr_prot.enable=true \
     vendor.audio.feature.src_trkn.enable=true \
+    vendor.audio.feature.spkr_prot.enable=true \
     vendor.audio.feature.ssrec.enable=true \
     vendor.audio.feature.usb_offload.enable=true \
     vendor.audio.feature.usb_offload_burst_mode.enable=false \
     vendor.audio.feature.usb_offload_sidetone_volume.enable=false \
+    vendor.audio.feature.deepbuffer_as_primary.enable=false \
     vendor.audio.feature.vbat.enable=true \
     vendor.audio.feature.wsa.enable=false \
-    vendor.audio.flac.sw.decoder.24bit=true \
-    vendor.audio.hw.aac.encoder=true \
-    vendor.audio.keep_alive.disabled=false \
-    vendor.audio.offload.buffer.size.kb=64 \
-    vendor.audio.offload.gapless.enabled=true \
-    vendor.audio.offload.multiaac.enable=true \
-    vendor.audio.offload.multiple.enabled=false \
-    vendor.audio.offload.passthrough=false \
-    vendor.audio.offload.track.enable=true \
-    vendor.audio.parser.ip.buffer.size=262144 \
-    vendor.audio.record.multiple.enabled=false \
-    vendor.audio.safx.pbe.enabled=false \
-    vendor.audio.tunnel.encode=false \
-    vendor.audio.use.sw.alac.decoder=true \
-    vendor.audio.use.sw.ape.decoder=true \
-    vendor.audio.volume.headset.gain.depcal=true \
-    vendor.audio_hal.period_size=192
+    vendor.audio.feature.audiozoom.enable=false \
+    vendor.audio.feature.snd_mon.enable=true
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES +=  \
@@ -101,8 +109,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Codec2
 PRODUCT_PROPERTY_OVERRIDES +=  \
-   debug.stagefright.omx_default_rank.sw-audio=1 \
-   debug.stagefright.omx_default_rank=0
+   debug.stagefright.omx_default_rank=0 \
+   debug.stagefright.ccodec=1
    
 # Display
 PRODUCT_PROPERTY_OVERRIDES +=  \
@@ -116,6 +124,10 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
 # FRP
 PRODUCT_PROPERTY_OVERRIDES +=  \
     ro.frp.pst=/dev/block/bootdevice/by-name/frp
+
+# Gatekeeper
+PRODUCT_PROPERTY_OVERRIDES +=  \
+   vendor.gatekeeper.disable_spu=true
 
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES +=  \
@@ -131,12 +143,26 @@ PRODUCT_PROPERTY_OVERRIDES +=  \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
 
+# Netflix custom property
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    ro.netflix.bsp_rev=Q670-14477-1
+
 # Network manager
 PRODUCT_PROPERTY_OVERRIDES +=  \
     persist.vendor.data.iwlan.enable=true \
     ro.telephony.iwlan_operation_mode=legacy
-    
-# Surfaceflinger properties    
+
+# NFC
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    ro.hardware.nfc_nci=nqx.default
+
+# Surfaceflinger
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    debug.sf.hw=0 \
+    persist.sys.sf.color_saturation=1.0 \
+    persist.sys.sf.native_mode=0
+
+# Surfaceflinger default propery overrides    
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.has_wide_color_display=true \
     ro.surface_flinger.has_HDR_display=true \
