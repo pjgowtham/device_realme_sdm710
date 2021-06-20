dc400dd53b90dc88db7edf23e359aa97d1521977
pjgowtham <pjgowtham@gmail.com>
2021-06-20 23:36:41 +0530

    realme_sdm710 : Shorten unifying script

--- realme_sdm710_unifying_script.sh
+++ realme_sdm710_unifying_script.sh
@@ -8,21 +8,6 @@
 # Get device codename
 RMX_PEAFOWL=$(getprop "ro.product.model")
 
-# Fix camera blobs for Realme 5 pro
-if [ "$RMX_PEAFOWL" = "RMX1901" ] ; then
-
-    mv /odm/RMX1901/* /odm
+# Fix device specific configs
+    mv /odm/$RMX_PEAFOWL/* /odm
     rm /odm/RMX*
-else
-if [ "$RMX_PEAFOWL" = "RMX1921" ] ; then    
-    mv /odm/RMX1921/* /odm
-    rm /odm/RMX*
-else
-if [ "$RMX_PEAFOWL" = "RMX1971" ] ; then
-    mv /odm/RMX1971/* /odm
-    rm /odm/RMX*
-else    
-if [ "$RMX_PEAFOWL" = "RMX1851" ] ; then
-    mv /odm/RMX1851/* /odm
-    rm /odm/RMX*
-fi
