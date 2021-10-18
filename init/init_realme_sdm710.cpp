b174a005f73324af258610f9345a5eedec6c6256
pjgowtham <pjgowtham@gmail.com>
2021-10-18 12:47:57 +0530

    realme_sdm710 : Fix init

--- init/init_realme_sdm710.cpp
+++ init/init_realme_sdm710.cpp
@@ -10,7 +10,6 @@
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 
-#include <sys/sysinfo.h>
 #include <android-base/properties.h>
 #include "property_service.h"
 #include "vendor_init.h"
@@ -90,7 +89,6 @@ void vendor_load_properties() {
 		}
 	    }
 	}
-}
 
     // Override all partitions' props
     string prop_partitions[] = { "", "odm.", "product.", "system.", "vendor." };
