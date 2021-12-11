90a01af8a55280061bc55ca1ec200fd7e1d1f02b
pjgowtham <pjgowtham@gmail.com>
2021-12-11 10:00:01 +0530

    realme_sdm710 : Build sensors@2.0.multihal from source

--- device.mk
+++ device.mk
@@ -391,7 +391,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service \
+    android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge
 
 # Soong namespaces
